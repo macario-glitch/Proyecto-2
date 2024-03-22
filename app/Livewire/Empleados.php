cacdb756dc68e96de451e64392172825f6730d08
Mario <barni115@hotmail.com>
2024-03-21 19:13:28 -0600

    Proyecto 2

--- app/Livewire/Empleados.php
+++ app/Livewire/Empleados.php
@@ -4,14 +4,102 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use App\Models\Empleado;
+use Illuminate\Support\Facades\Validator;
 
 class Empleados extends Component
 {
-    public $empleados;
+    public $empleados, $Nombre, $Apellidos, $Puesto, $Sueldo, $Sucursal, $id_empleado, $rules;
+    public $modal = false;
 
     public function render()
     {
         $this->empleados = Empleado::all();
         return view('livewire.empleados');
+        
     }
+
+    public function crear()
+    {
+        $this->limpiarCampos();
+        $this->abrirModal();
+    }
+
+    public function abrirModal()
+    {
+        $this->modal = true;
+    }
+
+    public function cerrarModal()
+    {
+        $this->modal = false;
+    }
+
+    public function limpiarCampos()
+    {
+        $this->Nombre = '';
+        $this->Apellidos = '';
+        $this->Puesto = 'Asistente';
+        $this->Sueldo = 0.0;
+        $this->Sucursal = '';
+        $this->id_empleado = '';
+    }
+
+    public function editar($id)
+    {
+        $empleado = Empleado::findOrFail($id);
+        $this->id_empleado = $id;
+        $this->Nombre = $empleado->Nombre;
+        $this->Apellidos = $empleado->Apellidos;
+        $this->Puesto = $empleado->Puesto;
+        $this->Sueldo = $empleado->Sueldo;
+        $this->Sucursal = $empleado->Sucursal;
+        $this->abrirModal();
+    }
+
+    public function borrar($id)
+    {
+        Empleado::find($id)->delete();
+        session()->flash('message', 'Registro del Empleado eliminado correctamente');
+    }
+
+    public function guardar()
+    {
+        $rules = [
+
+            'Nombre' => 'required|string|min:2|max:50|regex:/^[\p{L}\sñ]+$/',
+
+            'Apellidos' => 'required|string|min:2|max:150|regex:/^[\p{L}\sñ]+$/',
+
+            'Puesto' => 'required|string|max:50|in:Pastelero(a),Panadero(a),Decorador(a),Cajero(a),Asistente,Repartidor,Gerente',
+
+            'Sueldo' => 'required|numeric|min:50|max:25000',
+
+            'Sucursal' => 'required|string|max:100|regex:/^[\p{L}\sñ!&#1234567890\-]+$/',
+        
+        ];
+
+        $this->validate($rules);
+        
+        Empleado::updateOrCreate(['id'=>$this->id_empleado],
+            [
+                'Nombre' => $this->Nombre,
+                'Apellidos' => $this->Apellidos,
+                'Puesto' => $this->Puesto,
+                'Sueldo' => $this->Sueldo,
+                'Sucursal' => $this->Sucursal
+                
+            ]);
+         
+         session()->flash('message',
+            $this->id_empleado ? 'Actualización exitosa!' : 'Alta Exitosa!');
+         
+         $this->cerrarModal();
+         $this->limpiarCampos();
+
+
+    }
+
+    
+    
+
 }
--- resources/views/livewire/empleados.blade.php
+++ resources/views/livewire/empleados.blade.php
@@ -1,3 +1,55 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<x-slot name="header">
+    <h1 class="text-gray-900">Empleados de Pasteleria</h1>
+</x-slot>
+
+<div class="py-12">
+    <div class="max-w-7x1 mx-auto sm:px6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-x1 sm:rounded-lg px-4 py-4">
+
+        @if(session()->has('message'))
+            <div class="bg-teal-100 rounded-b text-teal-900 px-4 py-4 shadow-md my-3" role="alert">
+                <div class="flex">
+                    <div>
+                        <h4>{{ session('message')}}</h4>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        <button wire:click="crear()" class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 my-3">Nuevo</button>
+        @if($modal)
+            @include('livewire.crear')
+        @endif
+
+        <table class="table-fixed w-full table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellidos</th>
+                    <th>Puesto</th>
+                    <th>Sueldo</th>
+                    <th>Sucursal</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($empleados as $empleado)
+                <tr>
+                    <td>{{$empleado->id}}</td>
+                    <td>{{$empleado->Nombre}}</td>
+                    <td>{{$empleado->Apellidos}}</td>
+                    <td>{{$empleado->Puesto}}</td>
+                    <td>${{$empleado->Sueldo}}</td>
+                    <td>{{$empleado->Sucursal}}</td>
+                    <td class="border px-4 py-2 text-center">
+                        <button wire:click="editar({{$empleado->id}})" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4">Editar</button>
+                        <button wire:click="borrar({{$empleado->id}})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4">Borrar</button>
+                </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        </div>
+    </div>
 </div>
